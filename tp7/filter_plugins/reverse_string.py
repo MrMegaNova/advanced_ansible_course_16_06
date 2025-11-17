909f1384561152aa191b5de8e6548902ad64360f
Leo <leo.crotet@shellby.io>
2025-11-17 09:25:03 +0100

    first

--- tp7/filter_plugins/reverse_string.py
+++
@@ -1,8 +0,0 @@
-def reverse_string (strval):
-    return strval[::-1]
-
-class FilterModule(object):
-    def filters(self):
-        return {
-                "reverse_string":reverse_string
-                }
